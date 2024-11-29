ebb9c57e4c6694d9921519cbbe6bc893e7edb7a6
saharshg29 <87259675+saharshg29@users.noreply.github.com>
2024-11-30 02:55:07 +0530

    solved build errors 2

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,7 +4,7 @@ interface LayoutProps {
   children: React.ReactNode;
 }
 
-const Layout: React.FC<LayoutProps> = ({ children }) => {
+export const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-900 to-gray-800 text-white">
       <header className="border-b border-gray-700">
@@ -36,4 +36,3 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   )
 }
 
-export default Layout;
--- src/components/index.ts
+++
@@ -1,5 +0,0 @@
-// src/components/index.js or index.ts
-export { default as Label } from './ui/label';
-export { default as Checkbox } from './ui/checkbox';
-export { default as RadioGroup } from './ui/radio-group';
-// export { default as RadioGroupItem } from './ui/radio-group-item';
